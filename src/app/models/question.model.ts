7a4418a1337032c06c243a29ddc9babd6fcf946e
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-04-08 13:46:42 +0700

    Auto-generated commit

--- src/app/models/question.model.ts
+++
@@ -1,9 +0,0 @@
-export class Question {
-  text: string;
-  options: { text: string; isCorrect: boolean }[];
-
-  constructor(text: string, options: { text: string; isCorrect: boolean }[]) {
-    this.text = text;
-    this.options = options;
-  }
-}
